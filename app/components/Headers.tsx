e0adab625c7d1e5f873ab16b7ae23f36b58c480a
Terry <qtny.dev@gmail.com>
2025-01-12 11:34:59 +0800

    change page title and name

--- app/components/Headers.tsx
+++ app/components/Headers.tsx
@@ -1,7 +1,7 @@
 export function Headers() {
     return (
         <header className="font-semibold text-5xl text-yellow-600 pt-1 pb-8">
-            FeedMe Kitchen
+            SE Order Bot Demo
         </header>
     )
 }
